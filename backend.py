7e64e9094e2272f31bb3087971b91f40b3b37dde
donfortune <donfortunet.df@gmail.com>
2023-04-26 15:06:17 +0100

    completed the get data function

--- backend.py
+++ backend.py
@@ -1,5 +1,6 @@
 import requests
-api_key = 'xxxxxxxxxxxxxxxxxxx'
+api_key = '174db087491c0816ee287b06e80c2f97'
+
 
 
 
@@ -7,10 +8,19 @@ def get_data(place, days=None, type_=None):
     url = f'http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={api_key}'
     response = requests.get(url)
     content = response.json()
-    return content
+    filtered_data = content['list']
+    nr_values = 8 * days
+    filtered_data = filtered_data[:nr_values]
+    if type_ == 'Temperature':
+        filtered_data = [dict['main']['temp'] for dict in filtered_data]
+    if type_ == 'Sky':
+        filtered_data = [dict['weather'][0]['main'] for dict in filtered_data]
+
+    return filtered_data
+
 
 
 
 if __name__== '__main__':
-    print(get_data(place='tokyo'))
+    print(get_data(place='tokyo', days=3, type_='Sky'))
 
--- main.py
+++ main.py
@@ -8,6 +8,6 @@ days = st.select_slider('Forecast Days', options=[1, 2, 3, 4, 5], help='Select t
 option = st.selectbox('What do you want to view', ('Temperature', 'Sky'))
 st.subheader(f'{option} for the next {days} days in {city}')
 
-main_data = get_data(place, days, type_)
-figure = px.line(x=dates, y=temperatures, labels={'x':'Date', 'y': 'Temperature (C)' })
+main_data = get_data(place='tokyo', days=None, type_=None)
+figure = px.line(main_data, labels={'x': 'Date', 'y': 'Temperature (C)'})
 st.plotly_chart(figure)
